55400842b09cdf83149f258004a35b88aaad6201
kassianesmentz <kassimentz@gmail.com>
2017-01-17 22:40:47 -0200

    Primeira tela - listagem de pontos turísticos

--- MobileApp/ListaManager.swift
+++ MobileApp/ListaManager.swift
@@ -12,5 +12,23 @@ import SwiftyJSON
 
 class ListaManager: NSObject {
     
+    func loadListaMenu(callback: @escaping(
+        _ stringArr:[String]?, _ error: Error?) -> ()) {
+        
+        Alamofire.request("http://dev.4all.com:3003/tarefa").responseJSON {response
+            in
+            
+            var values = [String]()
+            
+            let json = JSON(data: response.data!)
+            
+            for stringInArray in json["lista"] {
+                let value = stringInArray.1.stringValue
+                values.append(value)
+            }
+            
+            callback(values, json.error)
+        }
     
+    }
 }
